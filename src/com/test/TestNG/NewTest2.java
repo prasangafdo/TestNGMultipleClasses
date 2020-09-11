fa88a3dbb919febf22cc4632550d0488e3528b62
Prasanga Fernando <prasangafdz@outlook.com>
2020-09-11 18:37:32 +0530

    added NewTest2 java class
    added the second file

--- src/com/test/TestNG/NewTest2.java
+++ src/com/test/TestNG/NewTest2.java
@@ -3,26 +3,26 @@ package com.test.TestNG;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class NewTest1 {
+public class NewTest2 {
  
 	//Make sure not to create the package if we are creating the package by the time of new creation of the TestNG xml files.
 	/*Created by Prasanga Fernando
 	 *On 2020.09.10
 	 */
 	
-	@Test ()
+	@Test
 	public void mainTestCase() {
 		Assert.assertEquals("test", "test");
 	}
-	@Test (dependsOnMethods= {"mainTestCase"})
+	@Test (priority = 1)
 	public void secondTestCase() {
 		Assert.assertEquals("test", "test");
 	}
-	@Test (dependsOnMethods= {"secondTestCase"})
+	@Test (priority = 2)
 	public void thirdTestCase() {
 		Assert.assertEquals("test", "test");
 	}
-	@Test (dependsOnMethods= {"thirdTestCase"})
+	@Test (priority = 3)
 	public void fourthTestCase() {
 		Assert.assertEquals("test", "test");
 	}
